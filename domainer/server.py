00bb14b56c2fb09bf9bef310249b87699c908dc3
Diogo <dutradda@gmail.com>
2018-03-04 01:16:13 -0300

    Redefing DDD structure

--- domainer/server.py
+++ domainer/server.py
@@ -1,12 +1,18 @@
-from connexion.apps.aiohttp_app import AioHttpApp
+import six
+
+if six.PY34:
+    from connexion.apps.aiohttp_app import AioHttpApp as DefaultApp
+else:
+    from connexion.apps.flask_app import FlaskApp as DefaultApp
 
 
 class DomainServer(object):
 
     def __init__(self, domain, app_cls=None, app_kwargs={}, api_kwargs={}):
         if app_cls is None:
-            app_cls = AioHttpApp
-            app_kwargs['only_one_api'] = True
+            app_cls = DefaultApp
+            if six.PY34:
+                app_kwargs['only_one_api'] = True
 
         spec = domain.get_swagger_spec()
 
